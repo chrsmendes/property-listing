0630a8bf74b6cb74e37030f31c65b659c73a2386
Christopher mendes <titolei.bossle@gmail.com>
2025-10-06 19:18:06 -0300

    Add Render Preview Gate workflow and update API docs for dynamic host and protocol

--- routes/api-docs.js
+++ routes/api-docs.js
@@ -7,7 +7,30 @@ const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
+const getProtocol = (req) => {
+	const forwardedProto = req.headers['x-forwarded-proto'];
+	if (forwardedProto) {
+		return forwardedProto.split(',')[0];
+	}
+	return req.protocol;
+};
+
+router.get('/swagger.json', (req, res) => {
+	const protocol = getProtocol(req).toLowerCase() === 'https' ? 'https' : 'http';
+	const dynamicDoc = JSON.parse(JSON.stringify(swaggerDocument));
+
+	dynamicDoc.host = req.get('host');
+	dynamicDoc.schemes = [protocol];
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send(dynamicDoc);
+});
+
 // API Docs Route
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve, swaggerUi.setup(null, {
+	swaggerOptions: {
+		url: '/api-docs/swagger.json'
+	}
+}));
 
 module.exports = router;
